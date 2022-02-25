3e09c428d7656326a7bc91bdb5675dd6928612e8
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2022-02-24 21:57:12 -0300

    Add files via upload

---
+++ exemplo1.java
@@ -0,0 +1,8 @@
+public class exemplo1{
+
+  public static void main (String[]args){
+
+    System.out.println("PALMEIRAS NÃO TEM MUNDIAL");
+    
+  }  
+}
---
+++ exemplo2.java
@@ -0,0 +1,20 @@
+public class exemplo2{
+
+  public static void main (String[]args){
+
+    int idade = 19;
+
+    //checando a idade
+
+    if(idade <18){
+
+      System.out.println("Que pena, você só pode beber Toddynho");
+  
+    }
+    else{
+      System.out.println("Parabéns, você pode beber Coca Cola!!");
+    }
+
+    
+  }  
+}
